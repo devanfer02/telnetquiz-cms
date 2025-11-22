770a5e802901aa87f25cc9c5ae9d091e352a5bfa
devanfer <ferrelvsc20@gmail.com>
2025-11-22 18:30:29 +0700

    re: change file structure and generate schema

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
-import appCss from '../styles.css?url'
+import appCss from '../css/styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
 import { SidebarProvider } from '@/components/ui/sidebar'
