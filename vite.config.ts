7cfcce597f237e080a5d43ad1322d38a2f0c3975
devanfer <ferrelvsc20@gmail.com>
2025-11-27 12:53:11 +0700

    deploy: add wrangler cf deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   plugins: [
     // this is the plugin that enables path aliases
+    cloudflare({viteEnvironment: { name: 'ssr' }}),
     AutoImport({
       dirs: ["./src/types"],
       dts: false,
